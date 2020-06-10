63400ad47f84d6851b08c2902ed1ddd4cbf5aabf
Aleksey Sviridkin <f@lex.la>
2020-06-10 15:01:30 +0300

    init

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	res, err := http.Get("https://playoverwatch.com/en-us/career/pc/Bob-29205")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find().Each(func(i int, s *goquery.Selection) {
+		rank := s.Find("div.competitive-rank-level").Text()
+		fmt.Printf("%s\n", rank)
+	})
+
+}
---
+++ parser/parser.go
@@ -0,0 +1,3 @@
+package parser
+
+const TankRaing string = "#overview-section > div > div.u-max-width-container.row.content-box.gutter-18 > div > div > div.masthead-player > div > div.competitive-rank > div:nth-child(1) > div:nth-child(2)"
